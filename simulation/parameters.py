41614cf9506248d9afd233836088ff6ef139a3ea
timothyfoldes <foldestim@hotmail.fr>
2025-12-01 09:29:50 -0500

    changed things

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-Nsteps = 100_000_000 # total simulation steps, all simulation in figures ran for 1_000_000_000 steps
-Nthermalization = 1_000_000
+Nsteps = 1_000_000_000 # total simulation steps, all simulation in figures ran for 1_000_000_000 steps
+Nthermalization = 5_000_000
 period = 100 # save every period steps for the FPT calculation (fast acquisition rate)
 periodall = 100000 # save every periodall steps for contact frequency calculation (slower acquisition rate)
 restartSimulationEveryLEFstep = 20
@@ -10,8 +10,8 @@ N = 1800
 M = 10
 probe_indices = np.arange(0, N*M-1, 180) # write check for probe_indices vs N
 
-col = 0.2
-steps = 10000
+col = 0.2 
+steps = 10000 # steps between two LEF updates. For steps=5000 sets extrusion speed to 1 kb/s for symetric LEFs
 GPU_ID = '0'
 
 processivity = 210
